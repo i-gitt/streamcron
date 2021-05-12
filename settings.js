e7e4ef4b49cb8c4e13320765b9f54ab88f50f152
i-gitt <mail4al@web.de>
2021-05-12 09:55:04 +0200

    documentation edit

---
+++ settings.js
@@ -0,0 +1,23 @@
+{
+	"channelinventory" : [
+		{id:414922194,hook:[0],type:"twitch"},
+		{id:73577820,hook:[0],type:"twitch"},
+		{id:20373128,hook:[0],type:"twitch"},
+		{id:51740365,hook:[0],type:"twitch"},
+		{id:183426245,hook:[0],type:"twitch"},
+		{id:61960500,hook:[0],type:"twitch"},
+		{id:14318730,hook:[0],type:"twitch"},
+		{id:123132692,hook:[0],type:"twitch"},
+		{id:25577633,hook:[0],type:"twitch"},
+		{id:51823450,hook:[0],type:"twitch"},
+		{id:39957735,hook:[0],type:"twitch"},
+		{id:542633521,hook:[0],type:"twitch"},
+		{id:"UCVOQN_Js8j_coQwXxdEGguw",hook:[0],type:"yt"},
+		{id:"UCKAS1e6_hkycg8rvLtvqbTQ",hook:[0],type:"yt"}
+	],
+	"filename" : "streams.js",
+	"apikey": '6ih37dbcg2tlkzzfjfke0elebv14d7',
+	"hooks" : [
+		"https://discord.com/api/webhooks/837821073348886599/cftQd5aJaT44GqCu4T5PQAwxOsA3zPBiYulYa0KMLHreRmDaFFo4ghx4e0y8IGlsBdLG"
+	]
+}
